153cfefbfa33ff9e228748a264bb459d397e5b53
Karim Yasser <karimyasser275@gmail.com>
2023-11-19 20:23:28 +0200

    update static code analysis batch file

--- ci-cd-tools/static-analysis.bat
+++ ci-cd-tools/static-analysis.bat
@@ -11,8 +11,9 @@ exit /b 1
 :get_args
 
 set "top_dir=%1"
+set "report=2> %1\errs.txt"
 
 set "cmd=C:\Program Files\Cppcheck\cppcheck"
-set "opt=--error-exitcode=1 --inline-suppr"
+set "opt=--enable=all --error-exitcode=1 --inline-suppr"
 
-"%cmd%" %opt% "%top_dir%"
+"%cmd%" %opt% "%top_dir%" %report%
